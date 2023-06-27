f24f4be952d14227e3122159802ae7aca5bbbc4f
ypezoa <y.pezoa22@gmail.com>
2023-06-26 23:33:42 -0400

    fix host from sonar

--- sonar.js
+++ sonar.js
@@ -2,7 +2,7 @@ import scanner from 'sonarqube-scanner'
 
 scanner(
   {
-    serverUrl: 'https://8355-45-162-133-12.ngrok-free.app',
+    serverUrl: 'https://352d-45-162-133-12.ngrok-free.app/',
     // token: 'squ_66b38de2dfa8d294e4f520152a2956087490b9ae',
     options: {
       'sonar.projectName': 'Todo App',
--- src/components/Login/Form/Form.jsx
+++ src/components/Login/Form/Form.jsx
@@ -1,105 +1,102 @@
 import { EmailIcon, ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
 import {
-    Box,
-    Button,
-    FormControl,
-    Heading,
-    IconButton,
-    Input,
-    InputGroup,
-    InputRightElement,
-    Text,
-    useColorModeValue,
-    Link,
-    VStack
+  Box,
+  Button,
+  FormControl,
+  Heading,
+  IconButton,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+  useColorModeValue,
+  Link,
+  VStack
 } from '@chakra-ui/react'
 import { Link as ReachLink } from 'react-router-dom'
 import { COLORS } from '../../../theme/colors/colors'
 import { useForm } from '../../../hooks/useForm'
 
 export const Form = () => {
-    const { actions, states, setters } = useForm({
-        email: '',
-        password: ''
-    })
+  const { actions, states, setters } = useForm({
+    email: '',
+    password: ''
+  })
 
-    const { email, password } = states.values
+  const { email, password } = states.values
 
-    const handleSubmit = () => {
-        console.log(states.values)
-    }
+  const handleSubmit = () => {
+    console.log(states.values)
+  }
 
-    return (
-        <Box style={{ marginTop: 20 }}>
-            <Heading>Ingresa a tu espacio</Heading>
-            <FormControl width={{ base: 'xs', md: 'md' }} mt={5}>
-                <VStack alignItems='left'>
-                    <InputGroup>
-                        <InputRightElement>
-                            <EmailIcon color="teal.500" />
-                        </InputRightElement>
-                        <Input
-                            id="#email"
-                            errorBorderColor="red.500"
-                            variant="outline"
-                            onChange={e => actions.handleChange(e)}
-                            name="email"
-                            type="email"
-                            placeholder="email de usuario"
-                        />
-                    </InputGroup>
-                    <InputGroup>
-                        <InputRightElement>
-                            <IconButton
-                                isDisabled={!password}
-                                onClick={() => setters.setShowPassword(!states.showPassword)}
-                                aria-label="Show password"
-                                variant="ghost"
-                            >
-                                {states.showPassword ? (
-                                    <ViewIcon color="teal.500" />
-                                ) : (
-                                    <ViewOffIcon color="teal.500" />
-                                )}
-                            </IconButton>
-                        </InputRightElement>
-                        <Input
-                            id="#password"
-                            errorBorderColor="red.500"
-                            variant="outline"
-                            onChange={e => actions.handleChange(e)}
-                            name="password"
-                            type={states.showPassword ? 'text' : 'password'}
-                            placeholder="contraseña"
-                        />
-                    </InputGroup>
-                    <Text pl={2} fontSize={12}>
-                        Aún no tienes cuenta?{' '}
-                        <Link
-                            color={useColorModeValue(COLORS.dark_blue, COLORS.success)}
-                            as={ReachLink}
-                            to="/register"
-                        >
-                            Registrate!
-                        </Link>
-                    </Text>
-                    <Box pt={1}>
-                        <Button
-                            w="full"
-                            _hover={false}
-                            color={COLORS.primary_text}
-                            backgroundColor={useColorModeValue(COLORS.dark_blue, COLORS.secondary)}
-                            isDisabled={!email || !password}
-                            onClick={handleSubmit}
-                            aria-label="Send Login"
-                        >
-                            Ingresar
-                        </Button>
-                    </Box>
-                </VStack>
-            </FormControl>
-        </Box>
-    )
+  return (
+    <Box style={{ marginTop: 20 }}>
+      <Heading>Ingresa a tu espacio</Heading>
+      <FormControl width={{ base: 'xs', md: 'md' }} mt={5}>
+        <VStack alignItems="left">
+          <InputGroup>
+            <InputRightElement>
+              <EmailIcon color="teal.500" />
+            </InputRightElement>
+            <Input
+              id="#email"
+              errorBorderColor="red.500"
+              variant="outline"
+              onChange={e => actions.handleChange(e)}
+              name="email"
+              type="email"
+              placeholder="email de usuario"
+            />
+          </InputGroup>
+          <InputGroup>
+            <InputRightElement>
+              <IconButton
+                isDisabled={!password}
+                onClick={() => setters.setShowPassword(!states.showPassword)}
+                aria-label="Show password"
+                variant="ghost">
+                {states.showPassword ? (
+                  <ViewIcon color="teal.500" />
+                ) : (
+                  <ViewOffIcon color="teal.500" />
+                )}
+              </IconButton>
+            </InputRightElement>
+            <Input
+              id="#password"
+              errorBorderColor="red.500"
+              variant="outline"
+              onChange={e => actions.handleChange(e)}
+              name="password"
+              type={states.showPassword ? 'text' : 'password'}
+              placeholder="contraseña"
+            />
+          </InputGroup>
+          <Text pl={2} fontSize={12}>
+            Aún no tienes cuenta?{' '}
+            <Link
+              color={useColorModeValue(COLORS.dark_blue, COLORS.success)}
+              as={ReachLink}
+              to="/register">
+              Registrate!
+            </Link>
+          </Text>
+          <Box pt={1}>
+            <Button
+              w="full"
+              _hover={false}
+              color={COLORS.primary_text}
+              backgroundColor={useColorModeValue(COLORS.dark_blue, COLORS.secondary)}
+              isDisabled={!email || !password}
+              onClick={handleSubmit}
+              aria-label="Send Login">
+              Ingresar
+            </Button>
+          </Box>
+        </VStack>
+      </FormControl>
+    </Box>
+  )
 }
 
 Form.displayName = 'Form'
